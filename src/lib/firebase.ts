02a8d8bc5a04c3d8c9d7b32ab9d66687717900af
bchirhart <brian.chirhart@gmail.com>
2025-08-02 21:56:45 -0500

    Removed auth keys from config and replaced with env variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 
 // THIS IS WHERE YOUR REAL KEYS WILL GO
 const firebaseConfig = {
-  apiKey: "AIzaSyDXhpqBDihbU6Fe444_5S1DkzaENmbmQgc",
-  authDomain: "options-forge.firebaseapp.com",
-  projectId: "options-forge",
-  storageBucket: "options-forge.firebasestorage.app",
-  messagingSenderId: "932106715919",
-  appId: "1:932106715919:web:0152c670badcad2ecbb47a",
-  measurementId: "G-XDYRPQSNY2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
